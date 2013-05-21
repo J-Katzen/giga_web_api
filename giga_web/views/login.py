0fdbfa6916e2a1c61d354c32dc733fbc026bfb55
J-Katzen <Alto50@gmail.com>
2013-05-21 13:58:07 -0400

    separated views and posts work

---
+++ giga_web/views/login.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from flask import request
+from giga_web import giga_web, crud_url
+import requests
+import bcrypt
+import json
+
+app = giga_web
+
+# login
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    email = (request.form['email']).lower()
+    pw = request.form['pw']
+    r = requests.get(crud_url + '/users/?where={"email":"' + email + '"}')
+    if r.status_code == requests.codes.ok:
+        res = r.json()
+        hashed = res['_items'][0]['pw']
+        if bcrypt.hashpw(pw, hashed) == hashed:
+            return json.dumps(res['_items'][0])
+        else:
+            return json.dumps({'error': 'Invalid password'})
+    else:
+        return json.dumps({'error': 'Could not query DB'})
---
+++ giga_web/views/register.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from flask import request
+from giga_web import giga_web, crud_url
+import requests
+import bcrypt
+import json
+
+app = giga_web
+
+
+@app.route("/register/user", methods=['POST'])
+def register_user():
+    email = (request.form['email']).lower()
+    pw = request.form['pw']
+    r = requests.get(crud_url + '/users/?where={"email":"' + email + '"}')
+    if r.status_code == requests.codes.ok:
+        res = r.json()
+        if len(res['_items']) == 0:
+            crypted = bcrypt.hashpw(pw, bcrypt.gensalt())
+            payload = {'data': {
+                       'email': email,
+                       'pw': crypted,
+                       'fb_login': False,
+                       't_login': False}}
+            reg = requests.post(crud_url + '/users/',
+                                data=json.dumps(payload),
+                                headers={'Content-Type': 'application/json'})
+
+            return json.dumps(reg.content)
+        else:
+            return json.dumps({'error': 'User exists'})
+    else:
+        return json.dumps({'error': 'Could not query DB'})
