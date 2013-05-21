3cd6690147f55eb3a2d8f11ad9ff6913499f25e5
J-Katzen <Alto50@gmail.com>
2013-05-21 00:58:44 -0400

    removed line

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from giga_web import giga_web
 
 if __name__ == '__main__':
-    # Heroku support: bind to PORT if defined, otherwise default to 5000.
     port = 5001
 
     giga_web.run(host='0.0.0.0', port=port)
