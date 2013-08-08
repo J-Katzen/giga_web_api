73c6d8263fd785bec8af44ec9773cbbf091ba57c
J-Katzen <alto50@gmail.com>
2013-08-08 19:01:35 -0400

    celery stuff

--- giga_web/settings.py
+++ giga_web/settings.py
@@ -1,4 +1,6 @@
+from kombu import Queue, Exchange
 import urllib
+
 
 class Config(object):
     DEBUG = False
@@ -19,6 +21,20 @@ class DevelopmentConfig(Config):
     REDIS = 'ec2-54-226-128-157.compute-1.amazonaws.com'
     REDIS_PORT = 6379
     REDIS_DB = 1
+    CELERY_DEFAULT_QUEUE = 'test'
+    CELERY_QUEUES = (
+        Queue('test-donation_confirms', Exchange('test-donation_confirms'), routing_key='test.#'),
+        Queue('test-update_project', Exchange('test-update_project'), routing_key='test.#'),
+        Queue('test-update_campaign', Exchange('test-update_campaign'), routing_key='test.#'),
+        Queue('test-update_leaderboard', Exchange('test-update_leaderboard'), routing_key='test.#'),
+    )
+    CELERY_DEFAULT_EXCHANGE = 'test'
+    CELERY_DEFAULT_ROUTING_KEY = 'test.default'
+    CELERY_ROUTES = {'giga_web.tasks.conirm_donation': {'queue': 'test-donation_confirms', 'routing_key': 'test.confirm'},
+                     'giga_web.tasks.update_project_post': {'queue': 'test-update_project', 'routing_key': 'test.project'},
+                     'giga_web.tasks.update_campaign_post': {'queue': 'test-update_campaign', 'routing_key': 'test.campaign'},
+                     'giga_web.tasks.update_leaderboard_post': {'queue': 'test-update_leaderboard', 'routing_key': 'test.leaderboard'}
+                     }
 
 
 class TestingConfig(Config):
