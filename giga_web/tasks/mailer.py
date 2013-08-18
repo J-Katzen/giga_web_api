cfef2424d61679176ec45c0284b31b59208b51a3
J-Katzen <alto50@gmail.com>
2013-08-18 17:54:01 -0400

    oops

--- giga_web/tasks/mailer.py
+++ giga_web/tasks/mailer.py
@@ -3,10 +3,10 @@ from giga_web import celery, celery_logger
 from giga_web.ext import SES_Mailer
 
 logger = celery_logger
-mailer = SES_Mailer()
 
 @celery.task
 def new_user_mail(email, hash, name=''):
+    mailer = SES_Mailer()
     logger.info('task new_user_mail called: args: %s %s %s' % (email, hash, name))
     res = mailer.send_new_user(email, hash, name)
     if 'error' in res:
@@ -16,6 +16,7 @@ def new_user_mail(email, hash, name=''):
 
 @celery.task
 def verified_mail(email, name=''):
+    mailer = SES_Mailer()
     logger.info('task verified_email called: args: %s %s' % (email, name))
     res = mailer.send_verified_email(email, name)
     if 'error' in res:
