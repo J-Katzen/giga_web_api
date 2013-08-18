cf7c2d8a9b01bb39504621ebed812903d37bfd53
J-Katzen <alto50@gmail.com>
2013-08-18 17:50:23 -0400

    ses mailer workkk

--- giga_web/tasks/mailer.py
+++ giga_web/tasks/mailer.py
@@ -3,12 +3,12 @@ from giga_web import celery, celery_logger
 from giga_web.ext import SES_Mailer
 
 logger = celery_logger
-
+mailer = SES_Mailer()
 
 @celery.task
 def new_user_mail(email, hash, name=''):
     logger.info('task new_user_mail called: args: %s %s %s' % (email, hash, name))
-    res = SES_Mailer.send_new_user(email, hash, name)
+    res = mailer.send_new_user(email, hash, name)
     if 'error' in res:
     	new_user_mail.delay(email, hash, name)
     return
@@ -17,7 +17,7 @@ def new_user_mail(email, hash, name=''):
 @celery.task
 def verified_mail(email, name=''):
     logger.info('task verified_email called: args: %s %s' % (email, name))
-    res = SES_Mailer.send_verified_email(email, name)
+    res = mailer.send_verified_email(email, name)
     if 'error' in res:
     	verified_mail.delay(email, name)
     return
