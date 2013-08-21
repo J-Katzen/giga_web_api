aef8ef4d3c43d11d10f253b6f1e36d3b444d4e46
J-Katzen <alto50@gmail.com>
2013-08-20 23:47:16 -0400

    confirm thing

--- giga_web/views/confirm.py
+++ giga_web/views/confirm.py
@@ -23,34 +23,38 @@ def confirm_client(client_perma, campaign_perma):
 
 
 def confirm_moravian(client_perma, cashnet_data):
-    if 'result' in cashnet_data:
-        res = cashnet_data['result']
-    elif '&result' in cashnet_data:
-        res = cashnet_data['&result']
-    if res == 0:
-        cl = helpers.generic_get('/clients/', client_perma)
-        cl_j = cl.json()
-        client_id = cl_j['_id']
-        email = cashnet_data['ref1val1']
-        trans_id = cashnet_data['tx']
-        client_trans_id = cashnet_data['ref2val1']
-        date = cashnet_data['effdate']
-        total = int(float(cashnet_data['amount1'])) * 100
-        today = format_date_time(mktime(datetime.utcnow().date().timetuple()))
-        tmr = format_date_time(mktime((datetime.utcnow().date() + timedelta(days=1)).timetuple()))
-        parm = {}
-        parm = {'where': '{"email":"%s", "client_id": "%s", "total_donated": %d, "confirmed": {"$exists": false }, "created": {"$gte": "%s", "$lte": "%s"}}' %
-                (email, client_id, total, today, tmr)}
-        r = requests.get(crud_url + '/donations/', params=parm)
-        rj = r.json()
-        if len(rj['_items']) > 0:
-            donation = rj['_items'][0]
-            donation['processor_trans_id'] = str(trans_id)
-            donation['client_trans_id'] = str(client_trans_id)
+    cl = helpers.generic_get('/clients/', client_perma)
+    cl_j = cl.json()
+    client_id = cl_j['_id']
+    email = cashnet_data['ref1val1']
+    trans_id = cashnet_data['tx']
+    client_trans_id = cashnet_data['ref2val1']
+    #date = cashnet_data['effdate']
+    total = int(float(cashnet_data['amount1'])) * 100
+    #created before now, but after yesterday
+    now = format_date_time(mktime(datetime.utcnow().timetuple()))
+    yesterday = format_date_time(mktime((datetime.utcnow() - timedelta(days=1)).timetuple()))
+    parm = {}
+    parm = {'where': '{"email":"%s", "client_id": "%s", "total_donated": %d, "confirmed": {"$exists": false }, "created": {"$gte": "%s", "$lte": "%s"}}' %
+            (email, client_id, total, yesterday, now)}
+    r = requests.get(crud_url + '/donations/', params=parm)
+    rj = r.json()
+    if len(rj['_items']) > 0:
+        donation = rj['_items'][0]
+        donation['processor_trans_id'] = str(trans_id)
+        donation['client_trans_id'] = str(client_trans_id)
+
+        if 'result' in cashnet_data:
+            res = cashnet_data['result']
+        elif '&result' in cashnet_data:
+            res = cashnet_data['&result']
+        if res == 0:
             donation['confirmed'] = format_date_time(mktime(datetime.utcnow().timetuple()))
-            donation['confirm_source'] = 'cashnet'
-            patched = helpers.generic_patch('/donations/', donation, donation['etag'])
-            confirm_donation.delay(donation)
+        else:
+            donation['denied'] = format_date_time(mktime(datetime.utcnow().timetuple()))
+        donation['confirm_source'] = 'cashnet'
+        patched = helpers.generic_patch('/donations/', donation, donation['etag'])
+        confirm_donation.delay(donation)
         return cashnet_data
     else:
         return {'error': 'bad transaction'}
