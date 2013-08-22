7b6144d2e94c3c1e4aaf642a166333ccb68da65c
J-Katzen <alto50@gmail.com>
2013-08-22 14:15:19 -0400

    pushing the fix

--- giga_web/views/confirm.py
+++ giga_web/views/confirm.py
@@ -23,7 +23,7 @@ def confirm_client(client_perma, campaign_perma):
 
 
 def confirm_moravian(client_perma, cashnet_data):
-    cl = helpers.generic_get('/clients/', client_perma)
+    cl = helpers.generic_get('/clients/', client_perma.lower())
     print cashnet_data
     cl_j = cl.json()
     client_id = cl_j['_id']
