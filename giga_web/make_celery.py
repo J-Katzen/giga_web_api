15f7ac8fe3066f1442aaff5a3fb297c9bcb49c46
J-Katzen <alto50@gmail.com>
2014-01-27 00:30:45 -0800

    celery

--- giga_web/make_celery.py
+++ giga_web/make_celery.py
@@ -3,7 +3,7 @@
 from celery import Celery
 
 def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery = Celery(app.import_name, broker=app.config['BROKER_URL'])
     celery.conf.update(app.config)
     TaskBase = celery.Task
 
