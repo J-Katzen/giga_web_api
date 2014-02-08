82c290b710c495bcd45e528d55e7eab68c68f3b8
J-Katzen <alto50@gmail.com>
2014-02-07 21:03:56 -0800

    fixing the stats call

--- giga_web/views/stats.py
+++ giga_web/views/stats.py
@@ -5,8 +5,8 @@ from giga_web.models import Project
 
 app = giga_web
 
-@app.route('/homepage/stats/')
-def hompage_stats(client_perma, campaign_perma, methods=['GET']):
+@app.route('/homepage/stats/', methods=['GET'])
+def hompage_stats():
 	all_total_raised = Project.objects.sum('total_raised')
 	all_avg = Project.objects.average('total_raised')
 	all_projs = len(Projects.objects)
