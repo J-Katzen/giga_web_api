26ac490ab1809e21ca1b40279609afd0f80a0934
J-Katzen <alto50@gmail.com>
2014-02-19 14:05:50 -0800

    added marketing list with many fields

--- giga_web/models/__init__.py
+++ giga_web/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User, UserStripeInfo, FBFriend
 from .project import Project, Reward, Post
 from .pledge import Pledge
 from .transaction import Transaction
+from .marketingList import MarketingList
---
+++ giga_web/models/marketing_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from giga_web import db
+from .user import User
+
+STATUSES = ('not contacted', 'converted', 'pledged', 'maybe', 'no')
+
+class Contact(db.EmbeddedDocument):
+    email = db.EmailField(required=True)
+    owners = db.ListField(db.EmailField())
+    fullname = db.StringField()
+    phone = db.StringField()
+    converted = db.BooleanField()
+    donated = db.IntField()
+    status = db.StringField(choices=STATUSES, default='not contacted')
+
+class MarketingList(db.Document):
+    contacts = db.ListField(db.EmbeddedDocumentField(Contact))
+    pledge_conversion = db.IntField()
+    convert_conversion = db.IntField()
+    total_donated = db.IntField()
+    owner = db.ReferenceField(User)
