e19bd3c10894ccf9fa564dabd12f8d5adce9deaa
J-Katzen <alto50@gmail.com>
2014-02-27 20:35:49 -0800

    email is now a request argument

--- giga_web/views/contactapi.py
+++ giga_web/views/contactapi.py
@@ -7,13 +7,15 @@ from flask.views import MethodView
 from flask import request
 
 class ContactAPI(MethodView):
-    def get(self, ml_id, email):
-        if email is None or ml_id is None:
+    def get(self, ml_id, email=None):
+        email = request.args.get('email', None)
+        if ml_id is None or email is None:
             return helpers.api_error('No Contact ID Provided!', 404), 404
         else:
             return MarketingList.objects.get_or_404(id=ml_id, contacts__email=email).select_related(1).to_json()
 
     def post(self, ml_id, email=None):
+        email = request.args.get('email', None)
         data = request.get_json(force=True, silent=False)
         contact = Contact(**data)
         if email is not None:
@@ -22,7 +24,8 @@ class ContactAPI(MethodView):
             ml = MarketingList.objects(id=ml_id).update(push__contacts=contact)
         return helpers.api_return('OK', datetime.utcnow(), ml.id, 'Contact')
 
-    def delete(self, ml_id, email):
+    def delete(self, ml_id, email=None):
+        email = request.args.get('email', None)
         if ml_id is None or email is None:
             return helpers.api_error('No Contact ID Provided!', 404), 404
         else:
