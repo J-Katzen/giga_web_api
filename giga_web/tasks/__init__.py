34f8aff95f97762b6dafb81bb53680ea799cd285
J-Katzen <alto50@gmail.com>
2014-03-03 14:23:04 -0800

    how did feedback mail get there

--- giga_web/tasks/__init__.py
+++ giga_web/tasks/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 from .confirm import confirm_donation, update_user_post, update_ref_user_post, update_campaign_post, update_leaderboard_post
-from .mailer import new_user_mail, verified_mail, info_mail, thank_you_mail, feedback_mail
+from .mailer import new_user_mail, verified_mail, info_mail, thank_you_mail
