ad5a3da9a3e93e7d04fd8b7e97826715ce3fabd7
J-Katzen <alto50@gmail.com>
2014-03-10 14:17:10 -0700

    testing socketio settings

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from giga_web import giga_web
+from giga_web import giga_web, socketio
 
 application = giga_web
 
 if __name__ == '__main__':
     port = 5001
-    application.run(host='0.0.0.0', port=port, debug=True)
+    #application.run(host='0.0.0.0', port=port, debug=True)
+    socketio.run(application, host='0.0.0.0', port=port)
--- giga_web/__init__.py
+++ giga_web/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from flask.ext.socketio import SocketIO
 from giga_web.ext.ses_mailer import SES_Mailer
 from make_celery import make_celery
 from settings import ProductionConfig, TestConfig
@@ -10,9 +11,11 @@ giga_web = Flask('giga_web')
 giga_web.config.from_object(TestConfig)
 celery_logger = get_task_logger('giga_web')
 celery = make_celery(giga_web)
-db = MongoEngine(giga_web)
 mailer = SES_Mailer(giga_web)
+db = MongoEngine(giga_web)
 db.init_app(giga_web)
+socketio = SocketIO(giga_web)
+#socketio.init_app(giga_web)
 #running celery requires this command:
 #celery worker -A giga_web.celery --autoscale=4,2 -Q test_queue
 
