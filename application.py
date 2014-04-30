d818a3249cf526ebedbad3c41542a10bc518d448
J-Katzen <alto50@gmail.com>
2014-04-30 15:54:18 -0700

    changed back to test settings

--- application.py
+++ application.py
@@ -2,5 +2,5 @@ from giga_web import giga_web
 application = giga_web
 
 if __name__ == '__main__':
-    port = 80
-    application.run(host='0.0.0.0', port=port)
+    port = 5000
+    application.run(host='0.0.0.0', port=port, debug=True)
